bbb8bb6455efa3151ac95c82e34121103cb10fe3
ajayb <ajayb@yahoo-inc.com>
2015-11-21 17:36:22 -0800

    formatted the code as per google style guide and added .gitignore file

--- src/main/java/com/colobu/zkrecipe/queue/DistributedDelayQueueExample.java
+++ src/main/java/com/colobu/zkrecipe/queue/DistributedDelayQueueExample.java
@@ -1,7 +1,5 @@
 package com.colobu.zkrecipe.queue;
 
-import java.util.Date;
-
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
 import org.apache.curator.framework.api.CuratorEvent;
@@ -15,75 +13,77 @@ import org.apache.curator.retry.ExponentialBackoffRetry;
 import org.apache.curator.test.TestingServer;
 import org.apache.curator.utils.CloseableUtils;
 
+import java.util.Date;
+
 public class DistributedDelayQueueExample {
-	private static final String PATH = "/example/queue";
+    private static final String PATH = "/example/queue";
 
-	public static void main(String[] args) throws Exception {
-		TestingServer server = new TestingServer();
-		CuratorFramework client = null;
-		DistributedDelayQueue<String> queue = null;
-		try {
-			client = CuratorFrameworkFactory.newClient(server.getConnectString(), new ExponentialBackoffRetry(1000, 3));
-			client.getCuratorListenable().addListener(new CuratorListener() {
-				@Override
-				public void eventReceived(CuratorFramework client, CuratorEvent event) throws Exception {
-					System.out.println("CuratorEvent: " + event.getType().name());
-				}
-			});
+    public static void main(String[] args) throws Exception {
+        TestingServer server = new TestingServer();
+        CuratorFramework client = null;
+        DistributedDelayQueue<String> queue = null;
+        try {
+            client = CuratorFrameworkFactory.newClient(server.getConnectString(), new ExponentialBackoffRetry(1000, 3));
+            client.getCuratorListenable().addListener(new CuratorListener() {
+                @Override
+                public void eventReceived(CuratorFramework client, CuratorEvent event) throws Exception {
+                    System.out.println("CuratorEvent: " + event.getType().name());
+                }
+            });
 
-			client.start();
-			QueueConsumer<String> consumer = createQueueConsumer();
-			QueueBuilder<String> builder = QueueBuilder.builder(client, consumer, createQueueSerializer(), PATH);
-			queue = builder.buildDelayQueue();
-			queue.start();
-			
-			for (int i = 0; i < 10; i++) {
-				queue.put("test-" + i, System.currentTimeMillis() + 10000);
-			}
-			System.out.println(new Date().getTime() + ": already put all items");
-			
-			
-			Thread.sleep(20000);
-			
-		} catch (Exception ex) {
+            client.start();
+            QueueConsumer<String> consumer = createQueueConsumer();
+            QueueBuilder<String> builder = QueueBuilder.builder(client, consumer, createQueueSerializer(), PATH);
+            queue = builder.buildDelayQueue();
+            queue.start();
 
-		} finally {
-			CloseableUtils.closeQuietly(queue);
-			CloseableUtils.closeQuietly(client);
-			CloseableUtils.closeQuietly(server);
-		}
-	}
+            for (int i = 0; i < 10; i++) {
+                queue.put("test-" + i, System.currentTimeMillis() + 10000);
+            }
+            System.out.println(new Date().getTime() + ": already put all items");
 
-	private static QueueSerializer<String> createQueueSerializer() {
-		return new QueueSerializer<String>() {
 
-			@Override
-			public byte[] serialize(String item) {
-				return item.getBytes();
-			}
+            Thread.sleep(20000);
 
-			@Override
-			public String deserialize(byte[] bytes) {
-				return new String(bytes);
-			}
+        } catch (Exception ex) {
 
-		};
-	}
+        } finally {
+            CloseableUtils.closeQuietly(queue);
+            CloseableUtils.closeQuietly(client);
+            CloseableUtils.closeQuietly(server);
+        }
+    }
 
-	private static QueueConsumer<String> createQueueConsumer() {
+    private static QueueSerializer<String> createQueueSerializer() {
+        return new QueueSerializer<String>() {
 
-		return new QueueConsumer<String>() {
+            @Override
+            public byte[] serialize(String item) {
+                return item.getBytes();
+            }
 
-			@Override
-			public void stateChanged(CuratorFramework client, ConnectionState newState) {
-				System.out.println("connection new state: " + newState.name());
-			}
+            @Override
+            public String deserialize(byte[] bytes) {
+                return new String(bytes);
+            }
 
-			@Override
-			public void consumeMessage(String message) throws Exception {
-				System.out.println(new Date().getTime() + ": consume one message: " + message);
-			}
+        };
+    }
 
-		};
-	}
+    private static QueueConsumer<String> createQueueConsumer() {
+
+        return new QueueConsumer<String>() {
+
+            @Override
+            public void stateChanged(CuratorFramework client, ConnectionState newState) {
+                System.out.println("connection new state: " + newState.name());
+            }
+
+            @Override
+            public void consumeMessage(String message) throws Exception {
+                System.out.println(new Date().getTime() + ": consume one message: " + message);
+            }
+
+        };
+    }
 }
